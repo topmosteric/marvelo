2e39b76ba9d4bb45141137d4e2d9a12cb7cfcff9
topmosteric <134070746+topmosteric@users.noreply.github.com>
2024-01-22 14:25:38 +0100

    UHGnsmjsnd0ifjug

--- src/MarvelData.js
+++ src/MarvelData.js
@@ -2,6 +2,18 @@ export const characters = [
   // Iron Man
   {
     name: "",
+    alias: "Pastor W.F Kumuyi",
+    abilities: [""],
+    team: "Racing",
+    img1: "Pastor W.F Kumuyi.jpg",
+    homeworld: "Street racing",
+    img3: "url_to_img3_for_Natasha_Stark",
+    description:
+      "Miles Morales, the teenage Spider-Man, has bio-electrokinesis powers, in addition to wall-crawling abilities. As a member of the Champions, Miles embodies the spirit of the Spider-Man legacy, fighting for equality and justice in the Marvel Universe while navigating the challenges of being a young hero.",
+  },
+ 
+  {
+    name: "",
     alias: "Archbishop Benson Idahosa",
     img1: "Archbishop Benson Idahosa.jpg",
     homeworld: "Street racing",
@@ -21,30 +33,8 @@ export const characters = [
     homeworld: "Online CO-OP, PvP",
     
   },
-  {
-    name: "",
-    alias: "Kathryn Kuhlman",
-    abilities: [""],
-    team: "Action",
-    img1: "kathryn kuhlman.jpg",
-    homeworld: "Online CO-OP, PvP",
-
-    description:
-      "James 'Rhodey' Rhodes, also known as War Machine, is a highly skilled pilot and a military expert. He operates a heavily armed powered armor suit that enhances his strength and combat capabilities. With a strong sense of duty and loyalty to his allies, Rhodey plays a crucial role in the Avengers' efforts to maintain peace and protect the world.",
-  },
+ 
   // Spider-Man
-  {
-    name: "",
-    alias: "Pastor W.F Kumuyi",
-    abilities: [""],
-    team: "Racing",
-    img1: "Pastor W.F Kumuyi.jpg",
-    homeworld: "Street racing",
-    img3: "url_to_img3_for_Natasha_Stark",
-    description:
-      "Miles Morales, the teenage Spider-Man, has bio-electrokinesis powers, in addition to wall-crawling abilities. As a member of the Champions, Miles embodies the spirit of the Spider-Man legacy, fighting for equality and justice in the Marvel Universe while navigating the challenges of being a young hero.",
-  },
-
   {
     name: "AJ Styles",
     alias: "Tl Osborn",
@@ -55,7 +45,29 @@ export const characters = [
     description:
       "Cindy Moon, codenamed Silk, is a skilled hero with the power of web-shooting and remarkable superhuman agility. She is an essential member of the Agents of Atlas, using her arachnid-like abilities to combat threats and safeguard the world.",
   },
-  
+  {
+    name: "",
+    alias: "Pastor Emmanuel",
+    img1: "IMG-20230731-WA0000(1).jpg",
+    homeworld: "Street racing",
+    abilities: [
+      "  ",
+      "Powered armor suit",
+      "Superhuman strength",
+    ],
+    team: "Racing"
+  },
+   {
+    name: "",
+    alias: "Kathryn Kuhlman",
+    abilities: [""],
+    team: "Action",
+    img1: "kathryn kuhlman.jpg",
+    homeworld: "Online CO-OP, PvP",
+
+    description:
+      "James 'Rhodey' Rhodes, also known as War Machine, is a highly skilled pilot and a military expert. He operates a heavily armed powered armor suit that enhances his strength and combat capabilities. With a strong sense of duty and loyalty to his allies, Rhodey plays a crucial role in the Avengers' efforts to maintain peace and protect the world.",
+  },
   
 ];
 
